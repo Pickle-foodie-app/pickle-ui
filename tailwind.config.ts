d6954e71cdf671d7c070df1b3e4dd26e616c297c
Josh Li <joshxli.io@gmail.com>
2024-04-03 08:41:34 +1100

    🔧: add theme to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,21 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary': "#D9F43D",
+      bg: {
+        "black": "#202124",
+        "dark-grey": "#434343"
+      },
+      'white': "#FFF",
+    },
+    boxShadow: {
+      sm: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 3px 16px 0 rgba(0, 0, 0, 0.06)",
+      lg: "0 3px 9px 0 rgba(0, 0, 0, 0.1), 0 8px 40px 0 rgba(0, 0, 0, 0.08)"
+    },
+    borderRadius: {
+      sm: "10px"
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
